fce9e67bf1dd0b0251fc6dc4b5d9121a4d9ec169
Mohammadreza <106070226+MAkhoondi@users.noreply.github.com>
2025-07-04 11:51:01 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './travelJournal/Main';
+import Header from './travelJournal/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main
+        img="./img/Fuji.png"
+        countryName="Japan"
+        link="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+        placeName="Mount Fuji"
+        date="Dates: 12 Jan, 2021 - 24 Jan, 2021"
+        info="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+      />
+      <Main
+        img="./img/Sydney.jpg"
+        countryName="Australia"
+        link="https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.213108,17z"
+        placeName="Sydney Opera House"
+        date="Dates: 27 May, 2021 - 8 Jun, 2021"
+        info="The Sydney Opera House is a world-famous architectural masterpiece located on the Sydney Harbour, known for its unique sail-like design and vibrant cultural performances."
+      />
+      <Main
+        img="./img/geirangerfjord.png"
+        countryName="Norway"
+        link="https://www.google.com/maps/place/Geirangerfjord/@62.1049113,7.0752134,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b5b6c6f:0x1b0f8b4b0f8b0f8b!8m2!3d62.1049113!4d7.0752134"
+        placeName="Geirangerfjord"
+        date="Dates: 01 Oct, 2021 - 18 Nov, 2021"
+        info="Geirangerfjord is a breathtaking fjord located in Norway, known for its steep cliffs, lush greenery, and spectacular waterfalls. It is a popular destination for nature lovers and is recognized as a UNESCO World Heritage site."
+      />
     </div>
   );
 }
